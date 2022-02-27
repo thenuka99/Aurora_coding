c660c3febd1db00641df20d03fdbea2de311eaf6
thenuka99 <janithherath2@gmail.com>
2022-02-27 19:05:15 +0530

    next #2

--- src/Rearrange_Kangaroos_by_height.java
+++ src/Rearrange_Kangaroos_by_height.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Rearrange_Kangaroos_by_height {
+public class Rearrange_Kangaroos_by_height {
+    public static void main(String[] args) {
+        int[][] array= {
+                {1, 5},
+                {13, 1},
+                {12, 100},
+                {12, 85} };
+
+        java.util.Arrays.sort(array, new java.util.Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return Double.compare(a[0], b[0]);
+            }
+        });
+
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 2; j++)
+                {
+                    System.out.printf("%d ", array[i][j]);
+                }
+                System.out.println();
+            }
+
+
+    }
 }
